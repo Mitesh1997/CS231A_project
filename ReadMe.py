608ec84e5d57caeaca74d59878c4875a958c114d
Mitesh1997 <71910961+Mitesh1997@users.noreply.github.com>
2020-10-02 10:51:34 +0530

    Update ReadMe.py

--- ReadMe.py
+++ ReadMe.py
@@ -1,3 +1,4 @@
 Data set link: https://github.com/udacity/self-driving-car/tree/master/annotations
 
-Edit the config file first, then use command 'python train.py' to train, then use command 'python test.py' to test.
+Edit the config File first, Then use command 'python train.py' to train, then use command 'python test.py' to test.
+That privet and self_driving master
